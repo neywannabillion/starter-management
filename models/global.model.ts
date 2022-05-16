be59737535f709db9d350758b4284e2c1d976309
neywannabillion <chatchai.ney@gmail.com>
2022-05-16 18:40:37 +0700

    - Edit drawer menu - Edit container body

--- models/global.model.ts
+++ models/global.model.ts
@@ -5,14 +5,3 @@ export interface AuthenProps {
 export interface MenuProps {
     _menuList?: any[];
 }
-
-// interface MenuList {
-//     menuName: String,
-//     url: String,
-//     icon: React.ReactNode,
-//     newTab: Boolean,
-//     isCollapse: Boolean,
-//     isSub: Boolean,
-//     showNotification: Boolean,
-//     subMenu?: any[]
-// }
